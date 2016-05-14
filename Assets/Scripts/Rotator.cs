f167c0cc9369aa3e279380daf939f66dd28e8d22
Dan Harasymiw <danharasymiw@gmail.com>
2016-05-14 19:08:59 -0400

    dsfjhksdfhjksdfjkhdsfjhkds

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 
 public class Rotator : MonoBehaviour {
+	
 	public float xRotationSpeed = 80;
 	public float yRotationSpeed = 50;
 	public float zRotationSpeed = 100;
@@ -10,3 +11,4 @@ public class Rotator : MonoBehaviour {
 		transform.Rotate (new Vector3 (xRotationSpeed, yRotationSpeed, zRotationSpeed) * Time.deltaTime);
 	}
 }
+sdfdfsgdfsdfssdfdsfdsfsdfsdfsdfasdasdasdasdasdsdfsdfsdfsdfsdfsdf
